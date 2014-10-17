9f27c50eaf051035cf5f09e32b8c73e7107fe7e1
Felix Langfeldt <felix@neverwoods.com>
2014-10-16 20:08:01 -0400

    Changed "strict" comparison to "loose" comparison
    Month names are tested using type and case sensitive checks. This is too strict and will fail if the case is slightly off. There is no real reason to check case and type.

--- js/bootstrap-datepicker.js
+++ js/bootstrap-datepicker.js
@@ -1536,7 +1536,7 @@
 			function match_part(){
 				var m = this.slice(0, parts[i].length),
 					p = parts[i].slice(0, m.length);
-				return m === p;
+				return m == p;
 			}
 			if (parts.length === fparts.length){
 				var cnt;
